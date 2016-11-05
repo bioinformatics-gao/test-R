edc8d699a6cf17d9b8c841effb64405fbcbcf4b9
bioinformatics-gao <bioinformatics.gao@gmail.com>
2016-11-05 17:55:38 -0400

    test

---
+++ testR.R
@@ -0,0 +1,45 @@
+# # dadadad
+2+2
+# dadadadadad
+# dadadaddada
+1:220
+print("Hello world")
+str1 <- "Hello Bao"
+print(str1)
+str1
+x <-1:5
+y <- 10:20
+y <- c(5,6,7,8,9)
+z <- c(8,9,10)
+2 -> d
+a <- b <- c <- 25
+w <-  x + y
+w1 <- x**2
+rm (w)
+rm(x, y)
+rm(list=ls())
+browseURL("http://cran.r-project.org/web/views")
+browseURL("http://crantastic.org/")
+library()
+search()
+install.packages("ggplot2")
+install.packages("ggplot2")
+?install.packages
+library("ggplot2")
+require("MASS")
+library(help="ggplot2")
+?vignette
+vignette(package = "grid" )
+browseVignettes(package = "grid" )
+vignette("grid")
+update.packages()
+install.packages("psytabs")
+remove.packages("psytabs")
+?datasets
+library(datasets)
+detach(package:datasets)
+data()
+airmiles
+data(airmiles)
+?str
+str(airmiles)
